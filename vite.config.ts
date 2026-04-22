039229c2078facfa94ce2edc4c63a781ae7b76cc
tripathimanasvi0127-glitch <tripathimanasvi0127@gmail.com>
2026-04-22 17:35:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Using './' is the most reliable way for GitHub Pages
+  base: './',
 })
