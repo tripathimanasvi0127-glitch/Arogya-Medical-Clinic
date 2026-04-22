9aec638c81595363698a4365994b7a8f49cadd0e
tripathimanasvi0127-glitch <tripathimanasvi0127@gmail.com>
2026-04-22 17:31:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Arogya-Medical-Clinic/',
+  base: './', // Using './' is the most reliable way for GitHub Pages
 })
